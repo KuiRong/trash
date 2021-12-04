abdfe639c4c655dc50220f199fad69b201d11a88
KuiRong <605470060@gms.tku.edu.tw>
2021-12-04 21:32:24 +0800

    Add circle function
    As tittle

---
+++ 天體運行.py
@@ -0,0 +1,34 @@
+import pyautogui
+import math
+
+coordinates_x, coordinates_y = pyautogui.size()
+
+coordinates_x /= 2
+coordinates_y /= 2
+
+Circle_radius = 300
+angle = 0
+
+print('coordinates_x is ' + str(coordinates_x))
+print('coordinates_y is ' + str(coordinates_y))
+
+try:
+    while True:
+
+        if angle == 360:
+            angle = 0
+            
+        pyautogui.moveTo(coordinates_x + Circle_radius* math.cos(angle/180*3.1415926),
+                         coordinates_y + Circle_radius *math.sin(angle/180*3.1415926),
+                         duration = 0.001)
+        angle += 1
+
+        x ,y = pyautogui.position()
+        positionStr = 'X: '+ str(x).rjust(4) + '  Y: '+ str(y).rjust(4) + '  angle: ' + str(angle).rjust(4)
+
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        print('\n')
+        
+except KeyboardInterrupt:
+    print('\nDone')
